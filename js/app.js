8cf94878c13cc43974f2c3e4ed6be77c909e3f0d
Henri <mail@henrihuisman.com>
2017-12-12 16:52:18 +0100

    worked on AJAX

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 $.ajax({
-  url: 'https://randomuser.me/api/?results=12/',
+  url: 'https://randomuser.me/api/?results=12',
   dataType: 'json',
   success: function(data) {
-    console.log(data);
+    var userContainer = document.querySelectorAll(".user-container");
+    $.each(data.results, function(i, user){
+    	userContainer += user.name.first;
+    });
+    $('.user-container').html(userContainer);
   }
 });
